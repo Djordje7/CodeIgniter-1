f59ca784a8c334411638ae111b1c068156e903a6
Djordje7 <54262324+Djordje7@users.noreply.github.com>
2019-09-24 08:36:55 +0200

    finish

--- application/views/pages/zugriff.php
+++ application/views/pages/zugriff.php
@@ -13,14 +13,14 @@
                               $summe_existing_now=0; ?>
                         <?php foreach ($zugriff as $rows) { ?>
                     <tr>             
-                            <?php$summe_zugriff_found+=$rows->zugriff_found
-                                $summe_still_missing+=$rows->not_found_and_still_missing
-                                $summe_existing_now+=$rows->not_found_but_existing_now?>
-
-                        <td><?=$rows->ludothek?></td>
-                        <td><?=$rows->zugriff_found?></td>
-                        <td><?=$rows->not_found_and_still_missing?></td>
-                        <td><?=$rows->not_found_but_existing_now?></td>
+                        <?php $summe_zugriff_found+=$rows->zugriff_found;
+                            $summe_still_missing+=$rows->not_found_and_still_missing;
+                            $summe_existing_now+=$rows->not_found_but_existing_now;?>
+                            
+                    <td><?=$rows->ludothek?></td>
+                    <td><?=$rows->zugriff_found?></td>
+                    <td><?=$rows->not_found_and_still_missing?></td>
+                    <td><?=$rows->not_found_but_existing_now?></td>
 				    </tr>
                 <?php } ?>  
             </table> 
