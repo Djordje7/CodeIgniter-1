e58745595a33a5fdbd78ff0a23be1086f4db6170
Djordje7 <54262324+Djordje7@users.noreply.github.com>
2019-09-24 08:34:25 +0200

    Update zugriff.php

--- application/views/pages/zugriff.php
+++ application/views/pages/zugriff.php
@@ -1,30 +1,38 @@
 <div class="container mt-5">
 	<div class="row">
 		<div class="col">
-
 			<table class="table">
 				<tr>
 					<th>Ludothek</th>
 					<th>Zugriff found</th>
 					<th>not_found_and_still_missing</th>
 					<th>not_found_but_existing_now</th>
-                        </tr>	
-                        <?php $summe=0;?>
-                        <?php $summe2=0;?>
-                        <?php $summe3=0;?>
+                </tr>	
+                        <?php $summe_zugriff_found=0;
+                              $summe_still_missing=0;
+                              $summe_existing_now=0; ?>
                         <?php foreach ($zugriff as $rows) { ?>
-                        <tr>
-                            
-                        <?php $summe+=$rows->zugriff_found?>
-                        <?php $summe2+=$rows->not_found_and_still_missing?>
-                        <?php $summe3+=$rows->not_found_but_existing_now?>
-                    <td><?=$rows->ludothek?></td>
-                    <td><?=$rows->zugriff_found?></td>
-                    <td><?=$rows->not_found_and_still_missing?></td>
-                    <td><?=$rows->not_found_but_existing_now?></td>
-				</tr>
+                    <tr>             
+                            <?php$summe_zugriff_found+=$rows->zugriff_found
+                                $summe_still_missing+=$rows->not_found_and_still_missing
+                                $summe_existing_now+=$rows->not_found_but_existing_now?>
+
+                        <td><?=$rows->ludothek?></td>
+                        <td><?=$rows->zugriff_found?></td>
+                        <td><?=$rows->not_found_and_still_missing?></td>
+                        <td><?=$rows->not_found_but_existing_now?></td>
+				    </tr>
                 <?php } ?>  
             </table> 
+            <div class="alert alert-primary" role="alert">
+                Es haben <?=$summe_zugriff_found?> versucht zugriff zu bekommen.
+            </div> 
+            <div class="alert alert-secondary" role="alert">
+                Es wurden <?=$summe_still_missing?> zugriffe nicht gefunden und weiterhin fehlend.
+            </div>
+            <div class="alert alert-dark" role="alert">
+                Es wurden <?=$summe_existing_now?> zugriffe nicht gefunden aber existieren nun.
+            </div>
         </div>
     </div>
 </div>
