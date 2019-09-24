576ef704eb62b71d86c44a96b96e697c5d8d6087
Djordje7 <54262324+Djordje7@users.noreply.github.com>
2019-09-24 10:29:58 +0200

    jo

--- application/views/pages/zugriff.php
+++ application/views/pages/zugriff.php
@@ -5,34 +5,35 @@
 				<tr>
 					<th>Ludothek</th>
 					<th>Zugriff found</th>
-					<th>not_found_and_still_missing</th>
-					<th>not_found_but_existing_now</th>
+					<th>nicht gefunden</th>
+					<th>wieder vorhanden</th>
                 </tr>	
-                        <?php $summe_zugriff_found=0;
-                              $summe_still_missing=0;
-                              $summe_existing_now=0; ?>
-                        <?php foreach ($zugriff as $rows) { ?>
+                <?php
+                    $summe_zugriff_found=0;
+                    $summe_still_missing=0;
+                    $summe_existing_now=0; 
+                
+                    foreach ($zugriff as $rows) { ?>
                     <tr>             
-                        <?php $summe_zugriff_found+=$rows->zugriff_found;
-                            $summe_still_missing+=$rows->not_found_and_still_missing;
-                            $summe_existing_now+=$rows->not_found_but_existing_now;?>
+                        <?php 
+                        $summe_zugriff_found+=$rows->zugriff_found;
+                        $summe_still_missing+=$rows->not_found_and_still_missing;
+                        $summe_existing_now+=$rows->not_found_but_existing_now;
+                        ?>
                             
-                    <td><?=$rows->ludothek?></td>
-                    <td><?=$rows->zugriff_found?></td>
-                    <td><?=$rows->not_found_and_still_missing?></td>
-                    <td><?=$rows->not_found_but_existing_now?></td>
-				    </tr>
-                <?php } ?>  
+                        <td><?=$rows->ludothek?></td>
+                        <td><?=$rows->zugriff_found?></td>
+                        <td><?=$rows->not_found_and_still_missing?></td>
+                        <td><?=$rows->not_found_but_existing_now?></td>
+                    </tr>
+                <?php } ?> 
+                <tr>
+                    <th>Die Gesamt summe:</th>
+                    <th><?=$summe_zugriff_found?></th>        
+                    <th><?=$summe_existing_now?></th>
+                    <th><?=$summe_still_missing?></th>      
+                </tr>    
             </table> 
-            <div class="alert alert-primary" role="alert">
-                Es haben <?=$summe_zugriff_found?> versucht zugriff zu bekommen.
-            </div> 
-            <div class="alert alert-secondary" role="alert">
-                Es wurden <?=$summe_still_missing?> zugriffe nicht gefunden und weiterhin fehlend.
-            </div>
-            <div class="alert alert-dark" role="alert">
-                Es wurden <?=$summe_existing_now?> zugriffe nicht gefunden aber existieren nun.
-            </div>
         </div>
     </div>
 </div>
