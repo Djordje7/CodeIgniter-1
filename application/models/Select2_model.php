6270d1f905999018ff0b298b37e61ce11eb2dde7
Stefan Bauer <sba@gmx.ch>
2019-09-25 09:08:57 +0200

    fix doc-block

--- application/models/Select2_model.php
+++ application/models/Select2_model.php
@@ -3,7 +3,7 @@
 class Select2_model extends CI_Model {
 
 	/**
-	 * @return list with all verlage for select2 ajax-data
+	 * @return list with all grouped items for select2 ajax-data
 	 */
 	public function get_verlage($search_term) {
 		$this->db->select("verlag AS id, verlag AS `text`");
@@ -16,7 +16,7 @@ class Select2_model extends CI_Model {
 	}
 
 	/**
-	 * @return list with all verlage for select2 ajax-data
+	 * @return list with all grouped items for select2 ajax-data
 	 */
 	public function get_azspieler($search_term) {
 		$this->db->select("azspieler AS id, azspieler AS `text`");
